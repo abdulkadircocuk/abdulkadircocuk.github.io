1ef30446a8040cdc21008ceed82e7151f899470f
abdulkadircocuk <108223345+abdulkadircocuk@users.noreply.github.com>
2022-06-26 14:47:33 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+/* ================= typing animation ================= */
+var typed = new Typed(".typing", {
+    strings:["","Web Designer","Web Developer","Graphic Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+
+})
+
+/* ================= Aside ================= */
+// const nav = document.querySelector(".nav"),
+//         navlist = nav.querySelector("li"),
+//         totalNavList = navlist.length;
+//         allSection = document.querySelectorAll(".section"),
+//         totalSection = allSection.length;
+//         for(let i=0; i<totalNavList; i++)
+//         {
+//             const a = navlist[i].querySelector("a");
+//             a.addEventListener("Click", function()
+//             {
+//                 for(let j=0; j<totalNavList; j++ )
+//                 {
+//                     navlist[j].querySelector("a").classList.remove("active");
+//                 }
+//                 this.classList.add("active")
+//                 showSection(this);
+//             })
+//         }
+
+//         function showSection(element)
+//         {
+//             for(let i=0; i<totalSection; i++)
+//             {
+//                 allSection[i].classList.remove("active");
+//             }
+//             const target = element.getAttribute("href").split("#")[1];
+//             document.querySelector("#" + target).classList.add("active")
+//         }
